e91004577728af7f64ce047908c199dc4a7b86cb
Developer-Zahid <coderexpertzahid@gmail.com>
2023-10-03 10:34:02 +0600

    Fixed Preloader issue & added extra info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
+/* 
+--------------------------------------------------------------------
+Template Name: 	    A2 Exterminators | Pest control company website
+Version:            1.00
+Author:             Zahid Hasan Munna | Developer Zahid
+Author Occupation: 	Frontend Web Design & Developer
+Author Whatsapp:    https://wa.me/8801992409030
+Author Linkedin:    https://www.linkedin.com/in/zahid-hasan-munna/
+Author Facebook:    https://www.facebook.com/coderexpertzahid/
+Author Github: 	    https://github.com/Developer-Zahid
+Author Website:     https://developer-zahid-portfolio.netlify.app/
+Author Email: 	    dgtaltechzahidhasan@gmail.com
+---------------------------------------------------------------------
+*/
+
 (function ($) {
     "use strict"
 
 	/* Preloader init */
 	function preLoader(){
-		$(".preloader").delay(400).fadeOut("slow");
+		$(".preloader").delay(300).fadeOut("slow");
 	}
 
 	/* Fix Header Height function */
